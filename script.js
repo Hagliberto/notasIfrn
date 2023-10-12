3ce9690a1a9e1ed8861c3295870e1818d3d7203d
Hagliberto <hagliberto@gmail.com>
2023-10-12 03:42:37 -0300

    Calculo de notas e validações de formulários

--- script.js
+++ script.js
@@ -1,85 +1,171 @@
-document.getElementById("botao-calcular-media").addEventListener("click", calcularMedia);
-document.getElementById("botao-calcular").addEventListener("click", calcularResultado);
+document
+  .getElementById("botao-calcular-media")
+  .addEventListener("click", calcularMedia);
+document
+  .getElementById("botao-calcular")
+  .addEventListener("click", calcularResultado);
 document.getElementById("botao-reset").addEventListener("click", reset);
 
+document.addEventListener("DOMContentLoaded", function () {
+  var notaInputs = document.getElementsByClassName("campo");
+  for (var i = 0; i < notaInputs.length; i++) {
+    notaInputs[i].addEventListener("click", function () {
+      this.removeAttribute("placeholder");
+    });
+  }
+});
+
+function validarNota(nota) {
+  return !isNaN(nota) && nota >= 0 && nota <= 10;
+}
+
 function calcularMedia() {
-    var notasAtividades = [];
-    for (var i = 1; i <= 8; i++) {
-        var nota = parseFloat(document.getElementById("nota-atividade-" + i).value);
-        if (!isNaN(nota)) {
-            notasAtividades.push(nota);
-        }
+  var notasAtividades = [];
+  var notasValidas = false;
+
+  for (var i = 1; i <= 8; i++) {
+    var campoNota = document.getElementById("nota-atividade-" + i);
+    var nota = parseFloat(campoNota.value.replace(",", "."));
+
+    if (validarNota(nota)) {
+      campoNota.classList.remove("campo-invalido");
+      notasAtividades.push(nota);
+      notasValidas = true;
+    } else {
+      campoNota.classList.add("campo-invalido");
     }
-    
-    if (notasAtividades.length === 0) {
-        alert("Por favor, insira as notas das atividades.");
-        return; // Interrompe a execução da função se as notas das atividades não forem inseridas
-    }
-    
-    var mediaAtividades = notasAtividades.reduce(function(acc, curr) {
-        return acc + curr;
+  }
+
+  if (!notasValidas) {
+    alert("Por favor, insira pelo menos uma nota válida entre 0 e 10.");
+    return;
+  }
+
+  var mediaAtividades =
+    notasAtividades.reduce(function (acc, curr) {
+      return acc + curr;
     }, 0) / notasAtividades.length;
-    
-    var porcentagemMedia = mediaAtividades * 0.4;
-    var notaProvaFinalNecessaria = (6 - porcentagemMedia) / 0.6;
-    
-    var resultadoElement = document.getElementById("resultado-media");
-    resultadoElement.innerHTML = "Média das Atividades: " + mediaAtividades.toFixed(2) + "<br>";
-    resultadoElement.innerHTML += "Porcentagem da Média: " + porcentagemMedia.toFixed(2) + "<br>";
-    resultadoElement.innerHTML += "Nota na Prova Final Necessária para Aprovação: " + notaProvaFinalNecessaria.toFixed(2);
+
+  if (mediaAtividades < 0 || mediaAtividades > 10) {
+    alert(
+      "A média das atividades está fora do intervalo válido (0-10). Por favor, verifique as notas das atividades."
+    );
+    return;
+  }
+
+  var porcentagemMedia = mediaAtividades * 0.4;
+
+  var resultadoElement = document.getElementById("parcial");
+  resultadoElement.innerHTML = "Nota Parcial: " + porcentagemMedia.toFixed(2);
+
+  // Exibir as informações no resultado-box
+  document.getElementById("media-atividades").textContent =
+    mediaAtividades.toFixed(2);
+  document.getElementById("parcial").textContent =
+    porcentagemMedia.toFixed(2);
+
+  var notaProvaFinalNecessaria = (6 - porcentagemMedia) / 0.6;
+  document.getElementById("nota-prova-final-necessaria").textContent =
+    notaProvaFinalNecessaria.toFixed(2);
 }
 
 function calcularResultado() {
-    var notasAtividades = [];
-    for (var i = 1; i <= 8; i++) {
-        var nota = parseFloat(document.getElementById("nota-atividade-" + i).value);
-        if (!isNaN(nota)) {
-            notasAtividades.push(nota);
-        }
-    }
-    
-    var notaProvaFinal = parseFloat(document.getElementById("nota-prova-final").value);
-    
-    if (isNaN(notaProvaFinal)) {
-        alert("Por favor, insira a nota da prova final.");
-        return; // Interrompe a execução da função se a nota da prova final não for inserida
-    }
-    
-    var notaFinal = 0;
-    if (notasAtividades.length > 0) {
-        var mediaAtividades = notasAtividades.reduce(function(acc, curr) {
-            return acc + curr;
-        }, 0) / notasAtividades.length;
-        
-        notaFinal = 0.4 * mediaAtividades + 0.6 * notaProvaFinal;
-    } 
-    
-    var resultadoElement = document.getElementById("resultado");
-    resultadoElement.innerHTML = "Nota Final: " + notaFinal.toFixed(2);
-    
-    if (notaFinal >= 6) {
-        resultadoElement.className = "aprovado";
-        resultadoElement.innerHTML += " - Aprovado";
-    } else if (notaFinal >= 4) {
-        resultadoElement.className = "recuperacao";
-        resultadoElement.innerHTML += " - Recuperação";
+  var notasAtividades = [];
+  var notasValidas = false;
+
+  for (var i = 1; i <= 8; i++) {
+    var campoNota = document.getElementById("nota-atividade-" + i);
+    var nota = parseFloat(campoNota.value.replace(",", "."));
+
+    if (validarNota(nota)) {
+      campoNota.classList.remove("campo-invalido");
+      notasAtividades.push(nota);
+      notasValidas = true;
     } else {
-        resultadoElement.className = "reprovado";
-        resultadoElement.innerHTML += " - Reprovado";
+      campoNota.classList.add("campo-invalido");
     }
+  }
+
+  var campoNotaProvaFinal = document.getElementById("nota-prova-final");
+  var notaProvaFinal = parseFloat(campoNotaProvaFinal.value.replace(",", "."));
+
+  if (validarNota(notaProvaFinal)) {
+    campoNotaProvaFinal.classList.remove("campo-invalido");
+    notasValidas = true;
+  } else {
+    campoNotaProvaFinal.classList.add("campo-invalido");
+  }
+
+  if (!notasValidas) {
+    alert("Por favor, insira pelo menos uma nota válida entre 0 e 10.");
+    return;
+  }
+
+  var notaFinal = 0;
+  if (notasAtividades.length > 0) {
+    var mediaAtividades =
+      notasAtividades.reduce(function (acc, curr) {
+        return acc + curr;
+      }, 0) / notasAtividades.length;
+
+    notaFinal = 0.4 * mediaAtividades + 0.6 * notaProvaFinal;
+  }
+
+  var resultadoElement = document.getElementById("final");
+  resultadoElement.innerHTML = "Nota Final: " + notaFinal.toFixed(2);
+
+  var notaFinalResultado = mediaAtividades * 0.4 + notaProvaFinal * 0.6;
+  var notaFinalArredondada = Math.round(notaFinalResultado * 100) / 100;
+
+  resultadoElement.innerHTML = "Sua nota é: " + notaFinalArredondada;
+
+  if (notaFinalArredondada >= 6) {
+    resultadoElement.className = "aprovado";
+    resultadoElement.innerText += "\nParabéns pela Aprovação!";
+  } else if (notaFinalArredondada >= 4 && notaFinalArredondada < 6) {
+    resultadoElement.className = "recuperacao";
+    resultadoElement.innerHTML += "<br>" + "Recuperação";
+  } else {
+    resultadoElement.className = "reprovado";
+    resultadoElement.innerHTML += "<br>" + "Reprovado";
+  }
 }
 
 function reset() {
     var inputs = document.getElementsByTagName("input");
+    var notasAtividadesVazias = true;
+    var notaProvaFinalVazia = true;
+  
     for (var i = 0; i < inputs.length; i++) {
-        inputs[i].value = "";
+      var campo = inputs[i];
+      campo.value = "";
+      campo.classList.remove("campo-invalido");
+  
+      if (campo.id.includes("nota-atividade-")) {
+        var nota = parseFloat(campo.value.replace(",", "."));
+        if (!isNaN(nota)) {
+          notasAtividadesVazias = false;
+        }
+      } else if (campo.id === "nota-prova-final") {
+        var notaProvaFinal = parseFloat(campo.value.replace(",", "."));
+        if (!isNaN(notaProvaFinal)) {
+          notaProvaFinalVazia = false;
+        }
+      }
     }
-    
-    var resultadoElement = document.getElementById("resultado-media");
-    resultadoElement.innerHTML = "";
-    resultadoElement.className = "";
-
-    var resultadoElement = document.getElementById("resultado");
-    resultadoElement.innerHTML = "";
-    resultadoElement.className = "";
-}
+  
+    if (notasAtividadesVazias && notaProvaFinalVazia) {
+      alert("Caso queira recalcular, insira pelo menos uma nota válida entre 0 e 10.");
+    }
+  
+    var elementsToClear = ["media-atividades", "parcial", "nota-prova-final-necessaria", "final"];
+  
+    elementsToClear.forEach(function (elementId) {
+      var resultadoElement = document.getElementById(elementId);
+      resultadoElement.innerHTML = "";
+      resultadoElement.className = "";
+    });
+  }
+  
+  
+  
